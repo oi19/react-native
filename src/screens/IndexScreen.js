610c6da112fd556ad992ed07aa11b6addb62524c
oi19 <you@example.com>
2022-02-26 16:13:02 +0200

    firt changes

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View, StyleSheet, } from 'react-native';
+
+
+const IndexScreen = () => {
+    return <View>
+        <Text>Index Screen </Text>
+    </View>
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
